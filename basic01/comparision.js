19ef1365e16dde74e55709c832950577f3aa4f55
Tarun Nagar <166895348+Tarunnagar03@users.noreply.github.com>
2025-01-30 10:49:01 +0000

    comparision

---
+++ basic01/comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 >1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 !=1);
+
+
+    /* chances to be stuck */
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+    //******* avoid these type of comparion because they will confuse ***** */
+console.log(null >0);
+console.log(null ==0);
+console.log(null >=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===   it check values and datatype strictly 
+
+console.log("2" === 2);
