8ff41761be0b077a49672aff8f173f862a0b82cb
Tarun Nagar <166895348+Tarunnagar03@users.noreply.github.com>
2025-01-30 09:41:32 +0000

    datatype study

---
+++ basic01/datatype.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3 + 3);   // code readability should be high
+
+console.log("tarun")
+
+
+let name ="tarun"
+let age = 20
+let isLoggedIn = false
+
+
+/*  premitive datatype */
+
+// number => 2 to pwr 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => stanalone value
+// undefined =>  value not assigned yet
+// symbol => unique
+
+// object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
