f996a24cadd62de0fb56bf05d1d57872cf49da6a
Tarun Nagar <166895348+Tarunnagar03@users.noreply.github.com>
2025-01-30 09:42:00 +0000

    conversion operation study

---
+++ basic01/conversionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+ // "33aab" => NaN
+// true/false => 1/0
+
+
+let isLoggedIn = "tarun"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false; 
+// " " => false ;
+//  tarun =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof  stringNumber);
